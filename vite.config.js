3706139e68ba11b82d73eb4dcf409e721a73e449
Rennerx3 <rmokosce21@hotmail.com>
2023-12-18 10:38:12 -0300

    cors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cors from 'koa-cors';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Rennerx3.github.io/Weather-App'
+  base: 'https://Rennerx3.github.io/Weather-App',
+  server: {
+    middleware: [cors()],
+  },
 })
